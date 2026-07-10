b08f64b195d4129dae4574569805444d2e0aa0e1
Alex123Palacin <152417813+Alex123Palacin@users.noreply.github.com>
2026-07-10 11:15:58 -0500

    Update LoginIniciarPages.tsx

--- FrontEnd/src/pages/LoginIniciarPages.tsx
+++ FrontEnd/src/pages/LoginIniciarPages.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import MenuModulosComp from "../components/MenuModulosComp";
 
 function LoginIniciarPages() {
   return (
-    <div>
-      <h1>Pantalla de Iniciar Sesión</h1>
+    <div className="flex min-h-screen w-full items-center justify-center bg-white">
+      <div
+        className="h-screen overflow-x-hidden overflow-y-auto border-4 border-black bg-[#dbeee8] p-4 font-[Arial]"
+        style={{
+          width: "30vw",
+          minWidth: "400px",/*360  */
+          maxWidth: "500px",/*430  */
+          wordBreak: "break-all",
+          overflowWrap: "anywhere",
+          whiteSpace: "normal",
+        }}
+      >
+        <h1>aaaaaaaaaaññññññññññññññññaaaaaaaaaaaaa</h1>
+        <MenuModulosComp />
+
+ 
+
+        
+      </div>
     </div>
   );
 }
